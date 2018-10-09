79bf9da07452b2d62d80de2cc0d32e8d29601455
oytuntutun <taurenswife@gmail.com>
2018-10-09 14:09:54 +0300

    still working
    deployment problems....

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <ReadTimer name='Cezmi' />
+        <ReadTimer name='no props' />
       </div>
     );
   }
